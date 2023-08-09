76ec82bbdca06384c5f1287093fe7f88e1cea96e
johndward01 <johndward01@gmail.com>
2023-08-09 11:42:43 -0500

    added book submit form with validations

---
+++ BooksStore/Models/SubmitBook.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksStore.Models;
+
+public class SubmitBook
+{
+    [Required]
+    [StringLength(80, MinimumLength = 3)]
+    public string? Title { get; set; }
+
+    [StringLength(5000)]
+    public string? Description { get; set; }
+
+    [Range(0, 9999)]
+    public int PagesCount { get; set; }
+
+    [Required]
+    [StringLength(80, MinimumLength = 3)]
+    public string? Author { get; set; }
+
+    [Range(typeof(decimal), "0", "99999")]
+    public decimal Price { get; set; }
+
+    public string? ISBN { get; set; }
+    public string Category { get; set; }
+}
