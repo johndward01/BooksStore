e5086ddbc7f64686a151e5c05f4fb4c4fc5053cc
johndward01 <johndward01@gmail.com>
2023-08-20 00:13:44 -0500

    added Login/out features + stopping point pg 251

---
+++ BooksStore/AuthorizationMessageHandler.cs
@@ -0,0 +1,24 @@
+﻿using Blazored.LocalStorage;
+
+namespace BooksStore;
+
+public class AuthorizationMessagingHandler : DelegatingHandler
+{
+    private readonly ILocalStorageService _localStorage;
+
+    public AuthorizationMessagingHandler(ILocalStorageService localStorage)
+    {
+        _localStorage = localStorage;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        if (await _localStorage.ContainKeyAsync("access_token"))
+        {
+            var token = await _localStorage.GetItemAsync<string>("access_token");
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+        }
+        return await base.SendAsync(request, cancellationToken);
+    }
+
+}
--- BooksStore/Program.cs
+++ BooksStore/Program.cs
@@ -3,25 +3,32 @@ using BooksStore;
 using BooksStore.Services;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Security.Claims;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped<AuthorizationMessageHandler>();
+builder.Services.AddHttpClient("BooksStore.ServerAPI", client => client.BaseAddress = new Uri(builder.Configuration["ApiUrl"]))
+    .AddHttpMessageHandler<AuthorizationMessageHandler>();
 
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiUrl"]) });
-builder.Services.AddScoped<DemoLoggingHandler>();
-builder.Services.AddHttpClient("BooksStore.API", httpClient =>
-    httpClient.BaseAddress = new Uri(builder.Configuration["ApiUrl"]))
-    .AddHttpMessageHandler<DemoLoggingHandler>();
-builder.Services.AddScoped(sp => 
-    sp.GetRequiredService<IHttpClientFactory>().CreateClient("BooksStore.API"));
-builder.Services.AddScoped<ILoggingService, ConsoleLoggingService>();
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BooksStore.ServerAPI"));
+
 builder.Services.AddScoped<IBooksService, BooksHttpClientService>();
+
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddSingleton<AppStateContainer>();
 builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
-builder.Services.AddAuthorizationCore();
+
+builder.Services.AddAuthorizationCore(options =>
+{
+    options.AddPolicy("UK_Customer", policy =>
+    {
+        policy.RequireClaim(ClaimTypes.Country, "UK");
+    });
+});
 builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
 
 await builder.Build().RunAsync();
